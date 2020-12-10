ed1e4d744edd7c6479faa6c6b05e16ad447b9deb
[akshitverma44] <[akshit@wednesday.is]>
2020-12-10 18:58:21 +0530

    Fragments Renamed

--- app/src/main/java/com/wednesday/banklocator/fragment/BankSearchFragment.kt
+++ app/src/main/java/com/wednesday/banklocator/fragment/BankSearchFragment.kt
@@ -4,4 +4,4 @@ import androidx.fragment.app.Fragment
 import com.wednesday.banklocator.R
 
 
-class SavedFragment : Fragment(R.layout.fragment_saved)
+class BankSearchFragment : Fragment(R.layout.fragment_search_bank)
