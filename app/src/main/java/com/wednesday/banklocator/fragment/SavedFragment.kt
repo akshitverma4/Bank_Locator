99529358c8d0cd9202557d9b3e831a758f702d63
[akshitverma44] <[akshit@wednesday.is]>
2020-12-10 18:29:44 +0530

    Directories

--- app/src/main/java/com/wednesday/banklocator/fragment/SavedFragment.kt
+++ app/src/main/java/com/wednesday/banklocator/fragment/SavedFragment.kt
@@ -1,6 +1,7 @@
-package com.wednesday.banklocator
+package com.wednesday.banklocator.fragment
 
 import androidx.fragment.app.Fragment
+import com.wednesday.banklocator.R
 
 
 class SavedFragment : Fragment(R.layout.fragment_saved)
