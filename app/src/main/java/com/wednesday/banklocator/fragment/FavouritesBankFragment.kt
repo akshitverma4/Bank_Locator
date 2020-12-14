1f3a87a4a8c8a26d161537cc217cbc71049adb23
Akshit Verma <59332509+akshitverma4@users.noreply.github.com>
2020-12-14 16:46:11 +0530

    Fragments renamed (#10)
    * Package Rename
    
    * Naminig Conventions
    
    * Directories
    
    * Fragments Renamed
    
    Co-authored-by: [akshitverma44] <[akshit@wednesday.is]>

--- app/src/main/java/com/wednesday/banklocator/fragment/FavouritesBankFragment.kt
+++ app/src/main/java/com/wednesday/banklocator/fragment/FavouritesBankFragment.kt
@@ -4,4 +4,4 @@ import androidx.fragment.app.Fragment
 import com.wednesday.banklocator.R
 
 
-class SearchFragment : Fragment(R.layout.fragment_search)
+class FavouritesBankFragment : Fragment(R.layout.fragment_favourites_bank)
