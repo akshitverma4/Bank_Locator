ec89f1bbc9a7de0533822cedd277441a726b9b74
Akshit Verma <59332509+akshitverma4@users.noreply.github.com>
2020-12-15 16:04:36 +0530

    Update IfscResponse.kt
    Changed the 'var' and 'val' names from capital to small letters.

--- app/src/main/java/com/wednesday/banklocator/model/IfscResponse.kt
+++ app/src/main/java/com/wednesday/banklocator/model/IfscResponse.kt
@@ -1,13 +1,13 @@
 package com.wednesday.banklocator.model
 
 data class IfscResponse(
-    val ADDRESS: String,
-    val BANK: String,
-    val BRANCH: String,
-    val CITY: String,
-    val CONTACT: String,
-    val DISTRICT: String,
-    var IFSC: String,
-    val RTGS: Boolean,
-    val STATE: String
+    val address: String,
+    val bank: String,
+    val branch: String,
+    val city: String,
+    val contact: String,
+    val district: String,
+    var ifsc: String,
+    val rtgs: Boolean,
+    val state: String
 )
