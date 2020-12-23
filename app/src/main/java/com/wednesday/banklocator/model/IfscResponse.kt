e5b5eb2d000445ce1bbb2266ccbef66e17843b44
Akshit Verma <59332509+akshitverma4@users.noreply.github.com>
2020-12-23 16:03:08 +0530

    Update IfscResponse.kt

--- app/src/main/java/com/wednesday/banklocator/model/IfscResponse.kt
+++ app/src/main/java/com/wednesday/banklocator/model/IfscResponse.kt
@@ -1,7 +1,6 @@
 package com.wednesday.banklocator.model
 
-import android.os.Parcel
-import android.os.Parcelable
+
 import androidx.room.Entity
 import androidx.room.Index
 import androidx.room.PrimaryKey
@@ -14,7 +13,7 @@ data class IfscResponse(
     @PrimaryKey(autoGenerate = true)
     var id: Int? = null,
     var ADDRESS: String="",
-    var BANK: String="Search",
+    var BANK: String="",
     var BRANCH: String="",
     var CITY: String="",
     var CONTACT: String="",
@@ -22,44 +21,6 @@ data class IfscResponse(
     var IFSC: String="",
     var RTGS: Boolean=false,
     var STATE: String=""
-):Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readInt()!!,
-        parcel.readString()!!,
-        parcel.readString()!!,
-        parcel.readString()!!,
-        parcel.readString()!!,
-        parcel.readString()!!,
-        parcel.readString()!!,
-        parcel.readString()!!,
-        parcel.readByte() != 0.toByte(),
-        parcel.readString()!!
-    )
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeInt(id!!)
-        parcel.writeString(ADDRESS)
-        parcel.writeString(BANK)
-        parcel.writeString(BRANCH)
-        parcel.writeString(CITY)
-        parcel.writeString(CONTACT)
-        parcel.writeString(DISTRICT)
-        parcel.writeString(IFSC)
-        parcel.writeByte(if (RTGS) 1 else 0)
-        parcel.writeString(STATE)
-    }
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<IfscResponse> {
-        override fun createFromParcel(parcel: Parcel): IfscResponse {
-            return IfscResponse(parcel)
-        }
-
-        override fun newArray(size: Int): Array<IfscResponse?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+)
+   
 
